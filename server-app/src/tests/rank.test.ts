b018876901e909b5c183759e6a2caa7f527a4927
Mohamed Elzohery <m.zohery1998@gmail.com>
2022-08-28 21:14:42 +0200

    Writing extra rank endpoint tests

--- server-app/src/tests/rank.test.ts
+++ server-app/src/tests/rank.test.ts
@@ -3,12 +3,13 @@ import app from '../app';
 
 interface TypedResponse extends Response{
     body: {
-        rank: number
+        rank?: number,
+        message?: string
     }
 };
 
 describe('Rank endpoint', () => {
-    let response: Response;
+    let response: TypedResponse;
     it('is equal to 80%',async () => {
         response = await request(app).post('/rank').send({finalScore: 90});
         expect(Array.isArray(response.body.rank)).toBe(90);
@@ -27,6 +28,30 @@ describe('Rank endpoint', () => {
     it('is equal to 26.67%',async () => {
         response = await request(app).post('/rank').send({finalScore: 30});
         expect(Array.isArray(response.body.rank)).toBe(26.67);
+    });
+
+    it('is invalid number -greater-',async () => {
+        response = await request(app).post('/rank').send({finalScore: 110});
+        expect(400);
+        expect(response.body.message).toBe('invalid final score');
+    });
+
+    it('is invalid number -lower-',async () => {
+        response = await request(app).post('/rank').send({finalScore: -10});
+        expect(400);
+        expect(response.body.message).toBe('invalid final score');
+    });
+
+    it('is an invalid value',async () => {
+        response = await request(app).post('/rank').send({finalScore: "random"});
+        expect(400);
+        expect(response.body.message).toBe('invalid final score');
+    });
+
+    it('is an empty body',async () => {
+        response = await request(app).post('/rank');
+        expect(400);
+        expect(response.body.message).toBe('invalid final score');
     });
 });
 
