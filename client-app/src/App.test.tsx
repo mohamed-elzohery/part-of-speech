87381559aaeba8d8e2081420aabb3a53e641e59d
Mohamed Elzohery <m.zohery1998@gmail.com>
2022-08-29 00:51:49 +0200

    yarn create react-app

---
+++ client-app/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Part of speech/i);
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ client-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './App.module.css';
+
+
+function App() {
+  return (
+   <div className={classes.container}>
+
+   </div>
+  );
+}
+
+export default App;
